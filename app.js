033cd236471582bb8873060095c89effbed55007
sreehari <sreeharivijayan619@gmail.com>
2017-06-25 22:47:27 +0530

    working-login-acheived

--- app.js
+++ app.js
@@ -5,42 +5,46 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var flash = require('connect-flash');
-var session= require('express-session');
-var passport= require('passport');
+var session = require('express-session');
+var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/learn');
+
+mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
+
 var routes = require('./routes/index');
-var users = require('./routes/users'); 
+var users = require('./routes/users');
 
-//init application
-
+// Init App
 var app = express();
-//view engine
-app.set('views',path.join(__dirname, 'views'));
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
-//body parser
+
+// BodyParser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(cookieParser()); 
-//set public folder -accessible to browser
-app.use(express.static(path.join(__dirname, 'public'))); 
-//express session middle vare
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
 app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: true }
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
 }));
 
-//passport init
+// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
-//express validator
+// Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -58,22 +62,26 @@ app.use(expressValidator({
   }
 }));
 
-//connect flash
+// Connect Flash
 app.use(flash());
-//global variables
-app.use(function(req, res, next) {
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	res.locals.error = req.flash('error');
-	next();
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
-app.use('/',routes);
-app.use('/users',users);
 
-//port
+
+app.use('/', routes);
+app.use('/users', users);
+
+// Set Port
 app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'),function(){
-	console.log('server-started on port '+app.get('port'));
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
 });
